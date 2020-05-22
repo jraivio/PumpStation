ce870983b85f24a291c5f59cff59f808c7e9f684
Jukka Raivio <juraivio@gmail.com>
2020-05-22 21:26:55 +0300

    Add files via upload

---
+++ Pumppu1/initSIM.ino
@@ -0,0 +1,11 @@
+  void initSIM() {
+  siminitstate = true;
+  digitalWrite(PWDPin, HIGH);
+  delay(5000);
+  if (gsm.begin(2400)) {
+    delay(5000);
+    simserialready = true;
+    siminitstate = false; 
+  if (debugging) { Serial.println("SIM serial = READY"); }
+  }
+}
